e9734b7874eb707774a60eb648e01528078a751b
cezou <cesairepro@gmail.com>
2024-06-07 10:31:43 +0200

    squid

---
+++ js/script.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+	var lang = window.location.search.split('=')[1];
+	if (!lang) {
+		lang = window.navigator.language.startsWith('fr') ? 'fr' : 'en';
+	} if (lang === 'fr') {
+		document.querySelector('.header-lang a[href="?lang=fr"]').classList.add('bold');
+	} else if (lang === 'en') {
+		document.querySelector('.header-lang a[href="?lang=en"]').classList.add('bold');
+	}
+	var translations = {
+		fr: {
+			"home.champagne": "NOS CHAMPAGNES",
+			"home.domaine": "NOTRE DOMAINE",
+			"home.approche": "NOTRE APPROCHE",
+			"home.actu": "ACTUALITÉS",
+			"home.contact": "NOUS CONTACTER"
+		},
+		en: {
+			"home.champagne": "OUR CHAMPAGNES",
+			"home.domaine": "OUR DOMAIN",
+			"home.approche": "OUR APPROACH",
+			"home.actu": "NEWS",
+			"home.contact": "CONTACT US"
+		}
+	};
+
+	for (var key in translations[lang]) {
+		document.querySelector('.header-menu a[href="#' + key + '"]').textContent = translations[lang][key];
+	}
+};
+
+window.addEventListener('DOMContentLoaded', (event) => {
+	fetch('html/header.html')
+		.then(response => response.text())
+		.then(data => {
+			document.querySelector('#header-container').innerHTML = data;
+		});
+});
